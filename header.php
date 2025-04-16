ab855d5ccefb929e03ad1862c56d1be59b613456
Ana Paula <ap.almeidapm@gmail.com>
2025-04-17 00:14:20 +0100

    desenvolvimento da pagina index

--- header.php
+++ header.php
@@ -9,41 +9,33 @@
     <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-      <ul class="navbar-nav  mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a style="color: white;" class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a style="color: white;" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Produtos
+    <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+      <div class="d-flex w-100 justify-content-between align-items-center">
+        <ul class="navbar-nav  mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#" role="button">
+              Produtos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Contactos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Minha Conta</a>
+          </li>
+        </ul>
+
+        <div class="d-flex ">
+          <i class="fa fa-search mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
+          <a href="./pages/login.php">
+            <i class="fa fa-user-circle-o mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Artesanatos</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </hr>
-            <li><a class="dropdown-item" href="#">Roupas</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Calçados</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a style="color: white;" class="nav-link" href="#">Contactos</a>
-        </li>
-        <li class="nav-item">
-          <a style="color: white;" class="nav-link" href="#">Minha Conta</a>
-        </li>
-      </ul>
-    </div>
-    <div class="d-flex ">
-      <i class="fa fa-search mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
-      <a href="./pages/login.php">
-        <i class="fa fa-user-circle-o mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
-      </a>
-      <i class="fa fa-cart-plus mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
+          <i class="fa fa-cart-plus mx-3" style="font-size:20px; color: white; margin-right: 20px;"></i>
+        </div>
+      </div>
     </div>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -1,19 +1,49 @@
+<?php
+include "header.php";
+include "config.php";
+
+$sql = "SELECT produtos.*, categorias.nome AS categoria_nome FROM produtos JOIN categorias ON produtos.categoria_id = categorias.id";
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cariri Shop</title>
-    <!--link bootstrap-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!--link font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cariri Shop</title>
+  <!--link bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!--link font awesome-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body class="p-3 m-0 border-0 bd-example m-0 border-0">
-        
-    <?php 
-      include "header.php";
-    ?>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <div class="container mt-4">
+    <h1>Produtos disponíveis</h1>
+    <div class="row">
+      <?php while ($produto = $result->fetch_assoc()): ?>
+        <div class="col-md-4 mb-4">
+          <div class="card h-100">
+            <img src="<?php echo $produto['imagem']; ?>" class="card-img-top" alt="Imagem do Produto" style="height: 300px; object-fit: cover;">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $produto['nome_produto']; ?></h5>
+              <p class="card-text"><?php echo $produto['descricao']; ?></p>
+              <p><strong>Categoria:</strong> <?php echo $produto['categoria_nome']; // depois pode fazer join com a tabela categorias 
+              ?></p>
+              <p><strong>Preço:</strong> R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
+              <p><strong>Estoque:</strong> <?php echo $produto['estoque']; ?></p>
+            </div>
+            <div class="card-footer d-flex justify-content-between">
+              <a href="carrinho.php?add=<?php echo $produto['id']; ?>" class="btn btn-primary">Comprar</a>
+            </div>
+          </div>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
